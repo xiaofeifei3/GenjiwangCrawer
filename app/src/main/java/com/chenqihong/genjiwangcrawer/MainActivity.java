ad1881da921736fd69171450da1322483f69af27
chenqihong <461687244@qq.com>
2017-11-23 17:03:14 +0800

    更新

--- app/src/main/java/com/chenqihong/genjiwangcrawer/MainActivity.java
+++ app/src/main/java/com/chenqihong/genjiwangcrawer/MainActivity.java
@@ -1,7 +1,16 @@
 package com.chenqihong.genjiwangcrawer;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
 
 import java.net.URL;
 import java.util.Iterator;
@@ -9,24 +18,76 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     public static final String URL = ".ganji.com/";
+    public WebView mWebView;
+    public Button mConfirmButton;
+    public Iterator<CityBean> mIterator;
+    public CityBean mCurrentCity;
+    public Runnable mRunnable = new Runnable() {
+        @Override
+        public void run() {
+            while(null != mCurrentCity || mIterator.hasNext()){
+                CityBean city = null;
+                if(null != mCurrentCity) {
+                    city = mCurrentCity;
+                    mCurrentCity = null;
+                }else{
+                    city = mIterator.next();
+                }
+
+                for(int index = 1; index <= 100; index++){
+                    try {
+                        Thread.sleep(2000);
+                        String raw = Crawer.syncGet("http://" + city.getPinyin().trim().toLowerCase() + URL + "danbaobaoxian/o" + index + "/");
+                        String result = Parser.parse(raw, city.getPinyin().trim().toLowerCase() + URL);
+                        if (null != result) {
+                            Message message = mHandler.obtainMessage();
+                            Bundle bundle = new Bundle();
+                            bundle.putString("url", result);
+                            message.setData(bundle);
+                            message.sendToTarget();
+                            mCurrentCity = city;
+                            return;
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    };
+
+    public Handler mHandler = new Handler(){
+        public void handleMessage(Message message){
+            Bundle bundle = message.getData();
+            String url = bundle.getString("url");
+            mWebView.getSettings().setJavaScriptEnabled(true);
+            mWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+            mWebView.getSettings().setSupportMultipleWindows(true);
+            mWebView.setWebViewClient(new WebViewClient());
+            mWebView.setWebChromeClient(new WebChromeClient());
+            mWebView.loadUrl(url);
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                List<CityBean> cityBeanList = City.getCityList();
-                Iterator<CityBean> i = cityBeanList.iterator();
-                while(i.hasNext()){
-                    CityBean city = i.next();
-                    for(int index = 1; index <= 100; index++){
-                        String raw = Crawer.syncGet("http://" + city.getPinyin().trim().toLowerCase() + URL + "danbaobaoxian/o" + index + "/");
-                        Parser.parse(raw, city.getPinyin().trim().toLowerCase() + URL);
+        initView();
+        new Thread(mRunnable).start();
+    }
 
-                    }
-                }
+    private void initView(){
+        mWebView = (WebView)findViewById(R.id.webview);
+        mConfirmButton = (Button)findViewById(R.id.confirmButton);
+        mConfirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(mRunnable).start();
             }
-        }).start();
+        });
+
+        List<CityBean> cityBeanList = City.getCityList();
+        mIterator = cityBeanList.iterator();
     }
 }
--- app/src/main/java/com/chenqihong/genjiwangcrawer/Parser.java
+++ app/src/main/java/com/chenqihong/genjiwangcrawer/Parser.java
@@ -1,5 +1,7 @@
 package com.chenqihong.genjiwangcrawer;
 
+import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 
 import java.io.*;
@@ -13,6 +15,8 @@ public class Parser {
     public static String TYPE_1_URL_TAG = "href=\"";
     public static String TYPE_1_URL_FINISH_TAG = "\" gjalog=";
     public static String TYPE_1_NAME_TAG_2 = "@position=title\">";
+    public static String TYPE_1_NAME_TAG_3 = "@position=title\'>";
+    public static String TYPE_1_NAME_TAG_4 = "@postion=title\">";
     public static String TYPE_1_NAME_FINISH_TAG = "</a>";
     public static String TYPE_1_PHONE_TAG = "@phone=";
     public static String TYPE_1_PHONE_FINISH_TAG = "@";
@@ -23,42 +27,61 @@ public class Parser {
     public static String TYPE_2_PHONE_TAG = "<span style=\"display: none;\" class=\"J_tel_phone_span\">";
     public static String TYPE_2_PHONE_FINISH_TAG = "</span>";
 
-    public static void parse(String raw, String mainUrl){
-        if(raw.contains(TYPE_1_NAME_TAG)){
+    public static String parse(String raw, String mainUrl) throws InterruptedException {
+        if(null == raw){
+            Log.e("blackList", "error3");
+        }
+
+        if(null != raw && raw.contains(TYPE_1_NAME_TAG)){
             String[] type1Names = raw.split(TYPE_1_NAME_TAG);
             for(int i = 1; i < type1Names.length; i++){
                 String url = type1Names[i].split(TYPE_1_URL_TAG)[1].split(TYPE_1_URL_FINISH_TAG)[0];
                 String phoneRaw = null;
-                if(2 > type1Names[i].split(TYPE_1_NAME_TAG_2).length){
-                    Log.e("blackList", "error" + type1Names[i]);
-                    return;
+                if(2 > type1Names[i].split(TYPE_1_NAME_TAG_2).length && 2 > type1Names[i].split(TYPE_1_NAME_TAG_3).length && 2 > type1Names[i].split(TYPE_1_NAME_TAG_4).length){
+                    Log.e("blackList", "error1" + type1Names[i]);
+                    return null;
                 }
 
-                String name = type1Names[i].split(TYPE_1_NAME_TAG_2)[1].split(TYPE_1_NAME_FINISH_TAG)[0];
+                String name = null;
+                if(2 < type1Names[i].split(TYPE_1_NAME_TAG_2).length ) {
+                    name = type1Names[i].split(TYPE_1_NAME_TAG_2)[1].split(TYPE_1_NAME_FINISH_TAG)[0];
+                }else if( 2 < type1Names[i].split(TYPE_1_NAME_TAG_3).length){
+                    name = type1Names[i].split(TYPE_1_NAME_TAG_3)[1].split(TYPE_1_NAME_FINISH_TAG)[0];
+                }else if(2 < type1Names[i].split(TYPE_1_NAME_TAG_4).length){
+                    name = type1Names[i].split(TYPE_1_NAME_TAG_4)[1].split(TYPE_1_NAME_FINISH_TAG)[0];
+                }
+
+                Thread.sleep(2000);
                 if(url.contains("http://") || url.contains("http://")) {
                     phoneRaw = Crawer.syncGet(url);
                 }else{
                     phoneRaw = Crawer.syncGet("http://" + mainUrl + url);
                 }
 
-                if(2 > phoneRaw.split(TYPE_1_PHONE_TAG).length){
-                    Log.e("blackList", "error" + phoneRaw);
-                    return;
+                if(null == phoneRaw || 2 > phoneRaw.split(TYPE_1_PHONE_TAG).length){
+                    if(null != phoneRaw && phoneRaw.contains("请输入验证码")){
+                        return url;
+                    }
+                    Log.e("blackList", "error2" + phoneRaw);
+
+                    return null;
                 }
                 String phone = phoneRaw.split(TYPE_1_PHONE_TAG)[1].split(TYPE_1_PHONE_FINISH_TAG)[0];
                 Log.e("blackList", name + "," + phone + "\n");
-                Writer.write(name + "," + phone + "\n");
+                Writer.write(phone + "\n");
             }
         }
 
-        if (raw.contains(TYPE_2_NAME_TAG)) {
+        if (null != raw && raw.contains(TYPE_2_NAME_TAG)) {
             String[] type2Names = raw.split(TYPE_2_NAME_TAG);
             for(int i=1; i < type2Names.length; i++){
                 String name = type2Names[i].split(TYPE_2_NAME_TAG_2)[1].split(TYPE_2_NAME_FINISH_TAG)[0];
                 String phone = type2Names[i].split(TYPE_2_PHONE_TAG)[1].split(TYPE_2_PHONE_FINISH_TAG)[0];
                 Log.e("blackList", name + "," + phone + "\n");
-                Writer.write(name + "," + phone + "\n");
+                Writer.write(phone + "\n");
             }
         }
+
+        return null;
     }
 }
